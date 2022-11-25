a41922f87491aa67bf173b9263163eb7c3ee4083
Thawan Camara <thawancamara@gmail.com>
2022-11-25 14:28:56 -0300

    created

---
+++ render_screen.c
@@ -0,0 +1,6 @@
+#include "tic_tac_toe.h"
+
+void	render_screen(t_game *game)
+{
+	system("clear");
+}
---
+++ tic_tac_toe.h
@@ -0,0 +1,12 @@
+#ifndef TIC_TAC_TOE_H
+# define TIC_TAC_TOE_H
+
+#include <stdio.h>
+
+typedef struct s_game
+{
+
+
+}	t_game;
+
+#endif
